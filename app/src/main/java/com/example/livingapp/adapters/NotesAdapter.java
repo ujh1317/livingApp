d5117f6dbf280f6e997c1c25be942b2e9b283ddb
Yujeonghyeon <dbwjdgus1317@gmail.com>
2020-07-08 01:32:43 +0900

    7/8 adapter

--- app/src/main/java/com/example/livingapp/adapters/NotesAdapter.java
+++ app/src/main/java/com/example/livingapp/adapters/NotesAdapter.java
@@ -1,5 +1,6 @@
 package com.example.livingapp.adapters;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -7,12 +8,26 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.livingapp.R;
+import com.example.livingapp.model.Note;
+
+import java.util.ArrayList;
+
 public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NoteHolder> {
+
+    private ArrayList<Note> notes;
+
+    public NotesAdapter(ArrayList<Note> notes) {
+        this.notes = notes;
+    }
 
     @NonNull
     @Override
     public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_layout, parent, false);
+
+        return new NoteHolder(v);
     }
 
     @Override
