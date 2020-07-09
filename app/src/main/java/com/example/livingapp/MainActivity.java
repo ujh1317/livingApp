79084c081ddfb54b73acf4d7b83876ce1c469a94
Yujeonghyeon <dbwjdgus1317@gmail.com>
2020-07-10 00:01:20 +0900

    7/10

--- app/src/main/java/com/example/livingapp/MainActivity.java
+++ app/src/main/java/com/example/livingapp/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.livingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+
+import com.example.livingapp.model.Note;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ArrayList<Note> notes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,4 +24,5 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
 }
