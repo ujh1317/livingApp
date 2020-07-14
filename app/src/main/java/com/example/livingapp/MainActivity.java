a5e01c5f1e1631e09079356aa4c9b1bb803c1d11
Yujeonghyeon <dbwjdgus1317@gmail.com>
2020-07-14 23:43:57 +0900

    7/14

--- app/src/main/java/com/example/livingapp/MainActivity.java
+++ app/src/main/java/com/example/livingapp/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.livingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.example.livingapp.adapters.NotesAdapter;
 import com.example.livingapp.model.Note;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,9 +29,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // init recyclerView
+        recyclerView = findViewById(R.id.notes_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
 
+        // init fab Button
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+                onAddNewNote();
+            }
+        });
+    }
+
+    private void loadNotes() {
+        this.notes = new ArrayList<>();
+        for (int i = 0; i < 12; i++) {
+            notes.add(new Note(" 입력하세요. ",
+                    new Date().getTime()));
+        }
+        adapter = new NotesAdapter(notes);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
 
     }
 
+    private void onAddNewNote() {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        loadNotes();
+    }
 }
