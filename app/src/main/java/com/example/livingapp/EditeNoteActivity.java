2b7a634c80d7e5de8a6a03402b51ca45aca0c7c8
Yujeonghyeon <dbwjdgus1317@gmail.com>
2020-07-18 16:34:50 +0900

    7/18 add activity

---
+++ app/src/main/java/com/example/livingapp/EditeNoteActivity.java
@@ -0,0 +1,14 @@
+package com.example.livingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditeNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edite_note);
+    }
+}
