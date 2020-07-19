bbc064ad4f3433edcdc26b363c76305b4ddd593d
Yujeonghyeon <dbwjdgus1317@gmail.com>
2020-07-19 22:10:05 +0900

    7/19

--- app/src/main/java/com/example/livingapp/EditeNoteActivity.java
+++ app/src/main/java/com/example/livingapp/EditeNoteActivity.java
@@ -3,12 +3,18 @@ package com.example.livingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class EditeNoteActivity extends AppCompatActivity {
+
+    private EditText inputNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edite_note);
+
+        inputNote = findViewById(R.id.input_note);
+
     }
 }
