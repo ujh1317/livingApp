84386352c3ede7ba1535c4de887bebf75f467b35
Yujeonghyeon <dbwjdgus1317@gmail.com>
2020-07-21 21:58:24 +0900

    7/21

--- app/src/main/java/com/example/livingapp/db/NotesDao.java
+++ app/src/main/java/com/example/livingapp/db/NotesDao.java
@@ -1,13 +1,20 @@
 package com.example.livingapp.db;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Update;
 
 import com.example.livingapp.model.Note;
 
 @Dao
 public interface NotesDao {
 
+    @Insert
     void insertNote(Note note);
+    @Delete
     void deleteNote(Note note);
+    @Update
+    void updateNote(Note note);
 
 }
